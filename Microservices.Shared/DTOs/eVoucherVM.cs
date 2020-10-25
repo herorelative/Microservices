3f9939facea2850041cf73ae4afc351c6f53bc41
Zar Ni Myo <herorelative@gmail.com>
2020-10-25 17:51:23 +0630

    eVoucher list endpoint added.

---
+++ Microservices.Shared/DTOs/eVoucherVM.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microservices.Shared.DTOs
+{
+    public class eVoucherVM
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime ExpireDate { get; set; }
+
+        public string Image { get; set; }
+
+        public double Amount { get; set; }
+
+        public Guid PaymentMethodId { get; set; }
+
+        public int DiscountOnPaymentMethod { get; set; }
+
+        public int Quantity { get; set; }
+
+        public BuyType BuyType { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
---
+++ Microservices.eStoreAPI/Controllers/EVouchersController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microservices.DataAccess.Repository.IRepository;
+using Microservices.Shared;
+using Microservices.Shared.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Microservices.eStoreAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EVouchersController : ControllerBase
+    {
+        private readonly IEVoucherRepo _eVoucherRepo;
+        private readonly IMapper _mapper;
+
+        public EVouchersController(IEVoucherRepo eVoucherRepo, IMapper mapper)
+        {
+            _eVoucherRepo = eVoucherRepo;
+            _mapper = mapper;
+        }
+
+        // GET: api/evouchers
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<eVoucherVM>>> GetAlleVouchers()
+        {
+            var eVouchers = await _eVoucherRepo.GetAlleVouchers();
+            return Ok(_mapper.Map<IEnumerable<eVoucherVM>>(eVouchers));
+        }
+
+        
+    }
+}
